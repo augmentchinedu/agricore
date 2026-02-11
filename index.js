a15008cde0de2ae3c8806057ef212124e74fd6ee
Augment Chinedu <46214862+augmentchinedu@users.noreply.github.com>
2026-02-11 20:49:22 +0000

    express installed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Agricore!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
